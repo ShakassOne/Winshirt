ab9cf87c74c30e4520fb69bd70bfad4443ae012c
ShakassOne <alan@shakass.com>
2025-07-31 18:19:16 +0200

    Initialisation propre du plugin WinShirt

---
+++ winshirt.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: WinShirt
+ * Description: Plugin WordPress pour personnalisation textile + loteries
+ * Version: 1.0.0
+ * Author: Alan Valensi
+ */
+if (!defined('ABSPATH')) exit;
