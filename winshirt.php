eba45c26e7638326e0fd0ba29d6cd9d26ed55ab7
ShakassOne <alan@shakass.com>
2025-08-14 19:04:48 +0200

    Update winshirt.php

--- winshirt.php
+++ winshirt.php
@@ -87,7 +87,7 @@ function winshirt_load_classes() {
     winshirt_require_if_exists( 'includes/class-winshirt-roadmap.php' );
     // winshirt_require_if_exists( 'includes/class-winshirt-settings.php' );
     winshirt_require_if_exists( 'includes/class-winshirt-order.php' );
-    // winshirt_require_if_exists( 'includes/class-winshirt-customizer.php' );
+    winshirt_require_if_exists( 'includes/class-winshirt-customizer.php' );
     // winshirt_require_if_exists( 'includes/class-winshirt-lottery.php' );
     // winshirt_require_if_exists( 'includes/class-winshirt-api.php' );
     
